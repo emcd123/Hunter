6c5e4af62818fa6d8f90e4c13d62ad935563672f
Owen McDonnell <Owen McDonnell@DESKTOP-4LTL6CN>
2019-02-21 20:41:40 +0000

    Added Monster model, Aswell as specific monster "kobold". Placed one in each room.

---
+++ Hunter/Core/Monster.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using RogueSharp;
+using RLNET;
+
+namespace Hunter.Core
+{
+    public class Monster : Actor
+    {
+        // Empty for now, we will add functionality later
+    }
+}
