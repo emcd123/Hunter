a71fd2e4cdafc93cf11e566491379730e84fc090
Owen McDonnell <Owen McDonnell@DESKTOP-4LTL6CN>
2019-03-05 23:33:40 +0000

    Refactored town map. Added Some buildings.

--- Hunter/MapGeneration/TownMap.cs
+++ Hunter/MapGeneration/TownMap.cs
@@ -16,6 +16,7 @@ namespace Hunter.Systems
     {
         private readonly int _width;
         private readonly int _height;
+        public List<Cell> DoorCoords;
 
         private readonly DungeonMap _map;
 
@@ -33,24 +34,132 @@ namespace Hunter.Systems
             // Initialize every cell in the map by
             // setting walkable, transparency, and explored to true
             _map.Initialize(_width, _height);
-            foreach (Cell cell in _map.GetAllCells())
+
+            //Make a list meant for the rooms
+            List<Rectangle> Rooms = new List<Rectangle>();
+
+            var FullRectangle = new Rectangle(0, 0, _width, _height);
+            MakeExteriorWall(_map, FullRectangle);
+            Rooms.Add(FullRectangle);            
+
+            for (int i = 0; i < Rooms.Count; i++)
             {
-                _map.SetCellProperties(cell.X, cell.Y, true, true, true);
+                Console.WriteLine(Rooms[i]);
+                MakeRoom(_map, Rooms[i]);
+                if (i != 0 || i != Rooms.Count)
+                    MakeDoor(_map, Rooms[i]);
+
+                PlaceSheriffsOffice(_map, Rooms[i]);//N
+                PlaceGunShop(_map, Rooms[i]);//S
+                PlaceGeneralStore(_map, Rooms[i]);//W
+                PlaceSaloon(_map, Rooms[i]);//E
+            }
+
+            PlacePlayer(_map, Rooms);
+            return _map;
+        }
+
+        private void MakeRoom(DungeonMap map, Rectangle Room)
+        {
+            foreach (Cell cell in map.GetAllCells())
+            {
+                map.SetCellProperties(cell.X, cell.Y, true, true, true);
             }
 
             // Set the first and last rows in the map to not be transparent or walkable
-            foreach (Cell cell in _map.GetCellsInRows(0, _height - 1))
+            foreach (Cell cell in map.GetCellsInRows(0, _height - 1))
             {
-                _map.SetCellProperties(cell.X, cell.Y, false, false, true);
+                map.SetCellProperties(cell.X, cell.Y, false, false, true);
             }
 
             // Set the first and last columns in the map to not be transparent or walkable
-            foreach (Cell cell in _map.GetCellsInColumns(0, _width - 1))
+            foreach (Cell cell in map.GetCellsInColumns(0, _width - 1))
             {
-                _map.SetCellProperties(cell.X, cell.Y, false, false, true);
+                map.SetCellProperties(cell.X, cell.Y, false, false, true);
             }
+        }
 
-            return _map;
+        private void PlaceSheriffsOffice(DungeonMap map, Rectangle Room)
+        {
+            //10x10 box of wall tiles with a door, north of the player
+            int centerX = Room.Center.X;
+            int centerY = Room.Center.Y;
+
+            int buildingCenter = centerY-25;
+            int doorCoord = buildingCenter + 5;
+
+            foreach (Cell cell in map.GetCellsInSquare(centerX, buildingCenter, 5))
+            {
+                map.SetCellProperties(cell.X, cell.Y, false, false, true);
+            }
+            MakeTownDoor(map, centerX, doorCoord);
+            //Rectangle building = new Rectangle(centerX - 5, doorCoord - 10, 10, 10);
+            //MakeBuilding(_map, building);
+        }
+
+        private void PlaceSaloon(DungeonMap map, Rectangle Room)
+        {
+            //10x10 box of wall tiles with a door, east of the player
+            int centerX = Room.Center.X;
+            int centerY = Room.Center.Y;
+
+            int buildingCenter = centerX + 25;
+            int doorCoord = buildingCenter - 5;
+
+            foreach (Cell cell in map.GetCellsInSquare(buildingCenter, centerY, 5))
+            {
+                map.SetCellProperties(cell.X, cell.Y, false, false, true);
+            }
+            MakeTownDoor(map, doorCoord, centerY);
+            //Rectangle building = new Rectangle(centerX - 5, doorCoord - 10, 10, 10);
+            //MakeBuilding(_map, building);
+        }
+
+        private void PlaceGeneralStore(DungeonMap map, Rectangle Room)
+        {
+            //10x10 box of wall tiles with a door, west of the player
+            int centerX = Room.Center.X;
+            int centerY = Room.Center.Y;
+
+            int buildingCenter = centerX - 25;
+            int doorCoord = buildingCenter + 5;
+
+            foreach (Cell cell in map.GetCellsInSquare(buildingCenter, centerY, 5))
+            {
+                map.SetCellProperties(cell.X, cell.Y, false, false, true);
+            }
+            MakeTownDoor(map, doorCoord, centerY);
+            //Rectangle building = new Rectangle(centerX - 5, doorCoord - 10, 10, 10);
+            //MakeBuilding(_map, building);
+        }
+
+        private void PlaceGunShop(DungeonMap map, Rectangle Room)
+        {
+            //10x10 box of wall tiles with a door, south of the player
+            int centerX = Room.Center.X;
+            int centerY = Room.Center.Y;
+
+            int buildingCenter = centerY + 25;
+            int doorCoord = buildingCenter - 5;
+
+            foreach (Cell cell in map.GetCellsInSquare(centerX, buildingCenter, 5))
+            {
+                map.SetCellProperties(cell.X, cell.Y, false, false, true);
+            }
+            MakeTownDoor(map, centerX, doorCoord);
+            //Rectangle building = new Rectangle(centerX - 5, doorCoord - 10, 10, 10);
+            //MakeBuilding(_map, building);
+        }
+
+        private void MakeTownDoor(DungeonMap map, int DoorCoordX, int DoorCoordY)
+        {
+            map.SetCellProperties(DoorCoordX, DoorCoordY, false, true, true);
+            map.Doors.Add(new Door
+            {
+                X = DoorCoordX,
+                Y = DoorCoordY,
+                IsOpen = false
+            });
         }
     }
 }
