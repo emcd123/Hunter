2a4bcbdd5d66ff5d521eb5486506ccae7a99a299
Owen McDonnell <Owen McDonnell@DESKTOP-4LTL6CN>
2019-04-03 20:03:04 +0100

    Removed item from map on pickup.

--- Hunter/Core/Inventory.cs
+++ Hunter/Core/Inventory.cs
@@ -12,21 +12,25 @@ namespace Hunter.Core
     {
         public List<Item> _inventory;
         
+        // Used when initialising the Player Object
         public static Inventory Empty()
         {
             return new Inventory() { _inventory = new List<Item>() };
         }
 
+        // When a player object already exists this is called, i.e on restart after death
         public void Reset()
         {
             _inventory = new List<Item>();
         }
 
+        // Meant for adding an item to the inventory when it does not require removal from the map
         public void AddItem(Item selected_item)
         {
             _inventory.Add(selected_item);
         }
 
+        // For taking an item off the map floor and adding to inventory
         public void PickUpItem(List<Item> item_list, Item selected_item)
         {
             _inventory.Add(selected_item);
@@ -61,7 +65,7 @@ namespace Hunter.Core
 
         private void RemoveFromMap(List<Item> item_list, Item selected_item)
         {
-
+            item_list.Remove(selected_item);
         }
 
         public void DrawEquipped()
